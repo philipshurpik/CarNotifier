9384d267628598efcbf6b5222e4a949875d5cedb
PhilipS <pylyp_Shurpyk@epam.com>
2015-07-30 16:02:43 +0300

    refactored promises

---
+++ app/phoneMock/data.js
@@ -0,0 +1,32 @@
+module.exports = {
+    user: {
+        userId: 1,
+        state: [10],
+        city: [0]
+    },
+    cars: [
+        {
+            userId: 1,
+            title: "BMW",
+            marka_id: 9,
+            model_id: 3219,
+            s_yers: 2005,
+            po_yers: 2013
+        },
+        {
+            userId: 1,
+            title: "Audi",
+            marka_id: 6,
+            model_id: 47,
+            s_yers: 2005,
+            po_yers: 2013
+        }, {
+            userId: 1,
+            title: "Alfa Romeo",
+            marka_id: 3,
+            model_id: 0,
+            s_yers: 2005,
+            po_yers: 2013
+        }
+    ]
+}
--- app/phoneMock/phoneMock.js
+++ app/phoneMock/phoneMock.js
@@ -1,78 +1,44 @@
 var LogMe = require('../util/logMe');
-var format = require('util').format;
 var mongo = require('promised-mongo');
+var Promise = require('bluebird');
+var data = require('./data');
 var db;
+var usersCollection;
+var carsCollection;
 
-function PhoneMock() {
-    db = mongo('carNotifier');
-}
+function PhoneMock() {}
 
 PhoneMock.prototype.init = function () {
+    db = mongo('carNotifier');
+    usersCollection = db.collection("users");
+    usersCollection.remove();
+    carsCollection = db.collection("cars");
+    carsCollection.remove();
+
     LogMe.log('phonemock init');
 
-    initUsers();
+    Promise.join(initUsers(), initCars(), function show(users, cars) {
+        LogMe.log(users);
+        LogMe.log(cars);
+    });
 };
 
 function initUsers() {
-    var userData = {
-        userId: 1,
-        state: [10],
-        city: [0]
-    };
+    return usersCollection.insert(data.user)
+        .then(getUsers);
 
-    var collection = db.collection("users");
-    collection.remove();
-    collection.insert(userData).then(function (result) {
-        collection.count().then(function (count) {
-            LogMe.log(format("users count = %s", count));
-        });
-
-        collection.find().toArray().then(function (results) {
-            LogMe.log(results);
-            initCars();
-        });
-    });
+    function getUsers() {
+        return usersCollection.find().toArray();
+    }
 }
 
 function initCars() {
-    var carsData = [
-        {
-            userId: 1,
-            title: "BMW",
-            marka_id: 9,
-            model_id: 3219,
-            s_yers: 2005,
-            po_yers: 2013
-        },
-        {
-            userId: 1,
-            title: "Audi",
-            marka_id: 6,
-            model_id: 47,
-            s_yers: 2005,
-            po_yers: 2013
-        }, {
-            userId: 1,
-            title: "Alfa Romeo",
-            marka_id: 3,
-            model_id: 0,
-            s_yers: 2005,
-            po_yers: 2013
-        }
-    ];
+    return carsCollection.insert(data.cars)
+        .then(getCars);
 
-    var collection = db.collection("cars");
-    collection.remove();
-    collection.insert(carsData).then(function (result) {
-        collection.count().then(function (count) {
-            LogMe.log(format("cars count = %s", count));
-        });
-
-        collection.find().toArray().then(function (results) {
-            LogMe.log(results);
-            db.close();
-        });
-    });
+    function getCars() {
+        return carsCollection.find().toArray();
+    }
 }
 
 module.exports = PhoneMock;
