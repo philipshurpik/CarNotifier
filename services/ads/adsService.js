c3214801660fe8d8dce5c9dce11cfd73b0390c94
philipshurpik <philip.shurpik@gmail.com>
2015-08-20 22:19:33 +0300

    ads service update

--- services/ads/adsService.js
+++ services/ads/adsService.js
@@ -1,15 +1,22 @@
 var router = require('express').Router();
 var ads = require('./ads.json');
+var adsCollection = app.db.adsCollection;
+var _ = require('lodash');
 
-router.get('/', function(req, res) {
-    var results = [];
-    ads.forEach(function(item) {
-        if (item.queryId === req.queryId) {
-            results.push(item);
-        }
-    });
+router.get('/new', function(req, res) {
+    var query = { userId: req.userId, carId: req.queryId, isViewed: false };
 
-    res.json(results);
+    adsCollection
+        .find(query)
+        .then(function(results) {
+            var ids = _.pluck(results, "_id");
+
+            adsCollection
+                .update({ _id: {$in: ids}}, {$set: {isViewed: true}}, {multi: true})
+                .then(function() {
+                    res.json({ads: results});
+                });
+        });
 });
 
 module.exports = router;
--- services/services.spec.js
+++ services/services.spec.js
@@ -4,3 +4,4 @@ var app = require('./services.js');
 
 require('./user/userService.spec.js');
 require('./car-query/carQueryService.spec.js');
+require('./ads/adsService.spec.js');
